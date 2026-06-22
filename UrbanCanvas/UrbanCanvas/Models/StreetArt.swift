bcaf1c03cffd69d6e38ab1e541902038bcadbd49
Issa Jaguraga <issa.jaguraga@oclock.school>
2026-06-22 19:15:56 +0200

    init: project setup with StreetArt model and ViewModel

---
+++ UrbanCanvas/UrbanCanvas/Models/StreetArt.swift
@@ -0,0 +1,15 @@
+//
+//  StreetArt.swift
+//  UrbanCanvas
+//
+//  Created by Issa on 22/06/2026.
+//
+
+import Foundation
+
+struct StreetArt : Identifiable {
+    var id = UUID()
+    var title: String
+    var image: String
+    var location: String
+}
---
+++ UrbanCanvas/UrbanCanvas/ViewModels/StreetArtViewModel.swift
@@ -0,0 +1,45 @@
+//
+//  StreetArtViewModel.swift
+//  UrbanCanvas
+//
+//  Created by Issa on 22/06/2026.
+//
+
+import Foundation
+import SwiftUI
+
+@Observable
+final class StreetArtViewModel {
+    var streetArts = [
+        StreetArt(title: "Liberté Égalité Fraternité",
+                  image: "",
+                  location: ""
+        ),
+        StreetArt(title: "La Joconde de Marseille",
+                  image: "",
+                  location: ""
+        ),
+        StreetArt(title: "La Maison de Cécile",
+                  image: "",
+                  location: ""
+        ),
+        StreetArt(title: "Chloé",
+                  image: "",
+                  location: ""
+        ),
+        StreetArt(title: "PA_278",
+                  image: "",
+                  location: ""
+        ),
+        StreetArt(title: "Chuuuut",
+                  image: "",
+                  location: ""
+        ),
+        StreetArt(title: "Le Tigre et la Cigale",
+                  image: "",
+                  location: ""
+        )
+    
+    
+    ]
+}
